35519c3c4141220962e3b3d05300663d60fb9b1c
bard <bartoszslesar@gmail.com>
2024-03-11 22:04:58 +0100

    project initialization

---
+++ src/main/java/com/example/cityweather/CityWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityWeatherApplication.class, args);
+    }
+
+}
